228d4fe7754c70f3d83ce8caa98f4b51897572bf
Will Roberts <wcrober@gmail.com>
2019-08-04 16:12:14 -0500

    Restructure Venom Website with BluePrints

--- venom/__init__.py
+++ venom/__init__.py
@@ -2,15 +2,38 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from venom.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'd5591cc74c68d8d2cff7f13a3f308357'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ymkafnkv:Sp0wyYklup8_qNYn-uAotIN4CUiwSk_L@otto.db.elephantsql.com:5432/ymkafnkv'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from venom import routes
+mail = Mail()
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from venom.users.routes import users
+    from venom.posts.routes import posts
+    from venom.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
+
+
